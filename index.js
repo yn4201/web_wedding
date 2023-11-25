54abf8d7296fcdd0269800eebb234ef510cefbd1
dnynhi <dnynhi.work@gmail.com>
2023-11-25 20:53:09 +0700

    push

---
+++ index.js
@@ -0,0 +1,13 @@
+document.getElementById("next").onclick = function () {
+  const widthItem = document.querySelector(".item").offsetWidth;
+  document.getElementById("formList").scrollLeft += widthItem;
+};
+document.getElementById("prev").onclick = function () {
+  const widthItem = document.querySelector(".item").offsetWidth;
+  document.getElementById("formList").scrollLeft -= widthItem;
+};
+
+
+document.querySelector("#header").addEventListener("click", function () {
+  overlay.classList.toggle("show");
+});
